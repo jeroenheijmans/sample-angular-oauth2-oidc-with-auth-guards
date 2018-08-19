f5e5b3897eec6a2fa7e517e440ac95b9b6c2c473
Jeroen Heijmans <github@jeroenheijmans.nl>
2018-08-19 17:02:00 +0200

    Narrow down function signature

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
-  ): Observable<boolean> | Promise<boolean> {
+  ): Observable<boolean> {
     return this.authService.canActivateProtectedRoutes$;
   }
 }
