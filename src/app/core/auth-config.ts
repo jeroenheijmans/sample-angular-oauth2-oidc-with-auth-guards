4c9244f8ed20a13d909e73e8d1a558c23e823652
Jan Verhaeghe <jan@hwfaq.be>
2020-02-24 09:40:02 +0100

    fix: semicolon encoding workaround

--- src/app/core/auth-config.ts
+++ src/app/core/auth-config.ts
@@ -11,5 +11,6 @@ export const authConfig: AuthConfig = {
   timeoutFactor: 0.25, // For faster testing
   sessionChecksEnabled: true,
   showDebugInformation: true, // Also requires enabling "Verbose" level in devtools
-  clearHashAfterLogin: false, // https://github.com/manfredsteyer/angular-oauth2-oidc/issues/457#issuecomment-431807040
+  clearHashAfterLogin: false, // https://github.com/manfredsteyer/angular-oauth2-oidc/issues/457#issuecomment-431807040,
+  nonceStateSeparator : 'semicolon' // Real semicolon gets mangled by IdentityServer's URI encoding
 };
