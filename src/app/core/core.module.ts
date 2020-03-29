6adeae0ef25e536d71ecc73e097538a9521cf211
Jeroen Heijmans <github@jeroenheijmans.nl>
2020-03-29 15:24:18 +0200

    Remove unneeded provider (for Code Flow)

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
-import { AuthConfig, NullValidationHandler, OAuthModule, OAuthModuleConfig, OAuthStorage, ValidationHandler, } from 'angular-oauth2-oidc';
+import { AuthConfig, OAuthModule, OAuthModuleConfig, OAuthStorage } from 'angular-oauth2-oidc';
 import { authConfig } from './auth-config';
 import { AuthGuardWithForcedLogin } from './auth-guard-with-forced-login.service';
 import { AuthGuard } from './auth-guard.service';
@@ -30,7 +30,6 @@ export class CoreModule {
       providers: [
         { provide: AuthConfig, useValue: authConfig },
         { provide: OAuthModuleConfig, useValue: authModuleConfig },
-        { provide: ValidationHandler, useClass: NullValidationHandler },
         { provide: OAuthStorage, useFactory: storageFactory },
       ]
     };
