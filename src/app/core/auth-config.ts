96387eb3955a388391df8cfa3ebcb752e3a8ac41
Jeroen Heijmans <github@jeroenheijmans.nl>
2020-03-29 15:23:28 +0200

    Document new option in config

--- src/app/core/auth-config.ts
+++ src/app/core/auth-config.ts
@@ -7,7 +7,7 @@ export const authConfig: AuthConfig = {
   redirectUri: window.location.origin + '/index.html',
   silentRefreshRedirectUri: window.location.origin + '/silent-refresh.html',
   scope: 'openid profile email api', // Ask offline_access to support refresh token refreshes
-  useSilentRefresh: true,
+  useSilentRefresh: true, // Needed for Code Flow to suggest using iframe-based refreshes
   silentRefreshTimeout: 5000, // For faster testing
   timeoutFactor: 0.25, // For faster testing
   sessionChecksEnabled: true,
