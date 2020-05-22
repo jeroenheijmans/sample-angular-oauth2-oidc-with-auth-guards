6eeb624ea0ea1106d47941721499ef202f95fd53
Jeroen Heijmans <github@jeroenheijmans.nl>
2020-05-22 12:09:55 +0200

    Fix compile errors on PR

--- src/app/core/auth-guard-with-forced-login.service.ts
+++ src/app/core/auth-guard-with-forced-login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { filter, map, tap } from 'rxjs/operators';
+import { filter, map, tap, withLatestFrom } from 'rxjs/operators';
 
 import { AuthService } from './auth.service';
 
@@ -20,9 +20,9 @@ export class AuthGuardWithForcedLogin implements CanActivate {
     state: RouterStateSnapshot,
   ): Observable<boolean> {
     return this.authService.isDoneLoading$
-      .pipe(withLatestFrom(this.authService.isAuthenticated$))
       .pipe(filter(isDone => isDone))
-      .pipe(tap([_, isAuthenticated] => isAuthenticated || this.authService.login(state.url)))
-      .pipe(map([_, isAuthenticated] => isAuthenticated));
+      .pipe(withLatestFrom(this.authService.isAuthenticated$))
+      .pipe(tap(([_, isAuthenticated]) => isAuthenticated || this.authService.login(state.url)))
+      .pipe(map(([_, isAuthenticated]) => isAuthenticated));
   }
 }
