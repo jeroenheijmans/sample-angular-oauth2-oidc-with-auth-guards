bd303c043724cf3468192db1e933551bd29d66d2
Jeroen Heijmans <github@jeroenheijmans.nl>
2020-08-10 20:46:45 +0200

    Add initial GitHub Actions workflow

---
+++ e2e/protractor-ci.conf.js
@@ -0,0 +1,13 @@
+const config = require('./protractor.conf').config;
+
+config.capabilities = {
+  browserName: 'chrome',
+  chromeOptions: {
+    args: [
+      '--headless',
+      '--no-sandbox',
+    ]
+  },
+};
+
+exports.config = config;
