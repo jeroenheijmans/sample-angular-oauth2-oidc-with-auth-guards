f5ac00389e686c4cc3fb1cb2f52687f33e64c1aa
Jeroen Heijmans <github@jeroenheijmans.nl>
2022-01-08 20:50:43 +0100

    Replace custom mock with Jasmine SpyObj
    Fixes #98 at the same time too

--- src/app/core/auth.service.spec.ts
+++ src/app/core/auth.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { EventType, OAuthService } from 'angular-oauth2-oidc';
-import { MockOAuthService } from '../../testing/mocks';
+import { EventType, OAuthEvent, OAuthService, OAuthSuccessEvent } from 'angular-oauth2-oidc';
+import { Subject } from 'rxjs';
 import { AuthService } from './auth.service';
 
 class FakeComponent {}
@@ -10,47 +10,63 @@ const loginUrl = '/should-login';
 
 describe('AuthService', () => {
   let service: AuthService;
-  let mockService: MockOAuthService;
+  let mockOAuthEvents: Subject<OAuthEvent>;
+  let mockOAuthService: jasmine.SpyObj<OAuthService>;
   let router: Router;
 
   beforeEach(() => {
+    mockOAuthEvents = new Subject<OAuthEvent>();
+    mockOAuthService = jasmine.createSpyObj<OAuthService>({
+      configure: void 0,
+      hasValidAccessToken: false,
+      loadDiscoveryDocument: Promise.resolve(new OAuthSuccessEvent('discovery_document_loaded')),
+      loadDiscoveryDocumentAndLogin: Promise.resolve(false),
+      loadDiscoveryDocumentAndTryLogin: Promise.resolve(false),
+      loadUserProfile: Promise.resolve({ }),
+      restartSessionChecksIfStillLoggedIn: void 0,
+      setupAutomaticSilentRefresh: void 0,
+      silentRefresh: Promise.resolve(new OAuthSuccessEvent('silently_refreshed')),
+      stopAutomaticRefresh: void 0,
+      tryLogin: Promise.resolve(false),
+      tryLoginCodeFlow: Promise.resolve(void 0),
+      tryLoginImplicitFlow: Promise.resolve(false),
+    }, {
+      events: mockOAuthEvents.asObservable(),
+    });
+
     TestBed.configureTestingModule({
       imports: [RouterTestingModule.withRoutes([
-        { path: 'should-login', component: FakeComponent }
+        { path: 'should-login', component: FakeComponent },
       ])],
       providers: [
         AuthService,
-        {provide: OAuthService, useClass: MockOAuthService}
+        { provide: OAuthService, useValue: mockOAuthService },
       ]
     });
 
     service = TestBed.inject(AuthService);
-    mockService = TestBed.inject(OAuthService) as any as MockOAuthService;
     router = TestBed.inject(Router);
     spyOn(router, 'navigateByUrl');
   });
 
   it('should react on OAuthService events', () => {
-    spyOn(mockService, 'hasValidAccessToken');
-    spyOn(mockService, 'loadUserProfile');
+    mockOAuthEvents.next({type: 'silently_refreshed'});
+    mockOAuthEvents.next({type: 'token_received'});
 
-    mockService.emulateEvent({type: 'silently_refreshed'});
-    mockService.emulateEvent({type: 'token_received'});
-
-    expect(mockService.loadUserProfile).toHaveBeenCalled();
-    expect(mockService.hasValidAccessToken).toHaveBeenCalledTimes(2);
+    expect(mockOAuthService.loadUserProfile).toHaveBeenCalled();
+    expect(mockOAuthService.hasValidAccessToken).toHaveBeenCalledTimes(2);
   });
 
   ['session_terminated', 'session_error'].forEach(eventType => {
     it(`should react on OAuthService event ${eventType} and redirect to login`, () => {
-      mockService.emulateEvent({type: eventType as EventType});
+      mockOAuthEvents.next({type: eventType as EventType});
 
       expect(router.navigateByUrl).toHaveBeenCalledWith(loginUrl);
     });
   });
 
   it('should handle storage event and update isAuthenticated status', () => {
-    mockService.updateTokenValidity(false);
+    mockOAuthService.hasValidAccessToken.and.returnValue(false);
 
     window.dispatchEvent(new StorageEvent('storage', {key: 'access_token'}));
 
@@ -60,38 +76,28 @@ describe('AuthService', () => {
 
   describe('runInitialLoginSequence', () => {
     it('should login via hash if token is valid', waitForAsync (() => {
-      spyOn(mockService, 'tryLogin');
-      spyOn(mockService, 'silentRefresh');
-      mockService.updateTokenValidity(true);
+      mockOAuthService.hasValidAccessToken.and.returnValue(true);
 
       service.runInitialLoginSequence().then(() => {
-        expect(mockService.tryLogin).toHaveBeenCalled();
-        expect(mockService.silentRefresh).not.toHaveBeenCalled();
+        expect(mockOAuthService.tryLogin).toHaveBeenCalled();
+        expect(mockOAuthService.silentRefresh).not.toHaveBeenCalled();
       });
     }));
 
     it('should silent login via refresh and navigate to state url when required user interaction', waitForAsync (() => {
-      spyOn(mockService, 'tryLogin');
-      spyOn(mockService, 'silentRefresh').and.returnValue(Promise.resolve({
-        type: 'silently_refreshed',
-        reason: {error: 'login_required'}
-      }));
-      mockService.state = '/some/url';
+      mockOAuthService.state = '/some/url';
 
       service.runInitialLoginSequence().then(() => {
-        expect(mockService.tryLogin).toHaveBeenCalled();
-        expect(mockService.silentRefresh).toHaveBeenCalled();
+        expect(mockOAuthService.tryLogin).toHaveBeenCalled();
+        expect(mockOAuthService.silentRefresh).toHaveBeenCalled();
         expect(router.navigateByUrl).toHaveBeenCalledWith('/some/url');
       });
     }));
 
     it('should silent login via refresh without redirect', waitForAsync (() => {
-      spyOn(mockService, 'tryLogin');
-      spyOn(mockService, 'silentRefresh');
-
       service.runInitialLoginSequence().then(() => {
-        expect(mockService.tryLogin).toHaveBeenCalled();
-        expect(mockService.silentRefresh).toHaveBeenCalled();
+        expect(mockOAuthService.tryLogin).toHaveBeenCalled();
+        expect(mockOAuthService.silentRefresh).toHaveBeenCalled();
         expect(router.navigateByUrl).not.toHaveBeenCalled();
       });
     }));
