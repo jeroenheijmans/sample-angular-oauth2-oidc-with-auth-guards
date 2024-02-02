40b7a92abc02d2112a8123df936608cba2264727
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-02-02 16:25:21 +0100

    Group steps for test as it was with Protractor

--- e2e/happy-path-flow.spec.ts
+++ e2e/happy-path-flow.spec.ts
@@ -1,71 +1,79 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 
 test.describe('Happy Path Flow', () => {
-  test('should start at home', async ({ page }) => {
+  let page: Page;
 
-    const clickMenuLinkTo = (route: string) => page.locator(`nav a[href="/${route}"]`).click();
-    const getMainContentText = () => page.locator('.container-fluid p.alert');
-    const getShownDebugValue = (item: string) => page.locator(`#${item}`);
+  test.describe.configure({ mode: 'serial' });
 
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+  });
+
+  test('should start at home', async () => {
     await page.goto("/");
     await expect(page.locator('h1')).toContainText('Welcome');
+  });
 
-    // should be able to navigate to a public page
-    await clickMenuLinkTo('basics/public');
+  test('should be able to navigate to a public page', async () => {
+    await page.locator(`nav a[href="/basics/public"]`).click()
     await expect(page.locator('h1')).toContainText('Welcome');
-    await expect(getMainContentText()).toContainText('🌐 PUBLIC');
+    await expect(page.locator('.container-fluid p.alert')).toContainText('🌐 PUBLIC');
+  });
 
-    // should see user is not logged in
-    await expect(getShownDebugValue('isAuthenticated')).toHaveText('false');
-    await expect(getShownDebugValue('hasValidToken')).toHaveText('false');
-    await expect(getShownDebugValue('isDoneLoading')).toHaveText('true');
-    await expect(getShownDebugValue('canActivateProtectedRoutes')).toHaveText('false');
+  test('should see user is not logged in at the start', async () => {
+    await expect(page.locator('#isAuthenticated')).toHaveText('false');
+    await expect(page.locator('#hasValidToken')).toHaveText('false');
+    await expect(page.locator('#isDoneLoading')).toHaveText('true');
+    await expect(page.locator('#canActivateProtectedRoutes')).toHaveText('false');
+  });
 
-    // should be able to navigate to IDS4
+  test('should be able to navigate to IDS4', async () => {
     await page.locator(".btn", { hasText: "login"}).click();
+  });
 
-    // should be able to log in on IDS4
+  test('should be able to log in on IDS4', async () => {
     await page.locator('input#Input_Username').fill('bob');
     await page.locator('input#Input_Password').fill('bob');
     await page.locator('button[value=login]').click();
+  });
 
-    // should initialize the application again
-    // (happens automatically in Playwright)
-
-    // should have silently refreshed and show being logged in
+  test('should have silently refreshed and show being logged in', async () => {
     await expect(page.locator('#email')).toHaveText('BobSmith@email.com');
+  });
 
-    // should show expected debug booleans
-    await expect(getShownDebugValue('isAuthenticated')).toHaveText('true');
-    await expect(getShownDebugValue('hasValidToken')).toHaveText('true');
-    await expect(getShownDebugValue('isDoneLoading')).toHaveText('true');
-    await expect(getShownDebugValue('canActivateProtectedRoutes')).toHaveText('true');
+  test('should show expected debug booleans', async () => {
+    await expect(page.locator('#isAuthenticated')).toHaveText('true');
+    await expect(page.locator('#hasValidToken')).toHaveText('true');
+    await expect(page.locator('#isDoneLoading')).toHaveText('true');
+    await expect(page.locator('#canActivateProtectedRoutes')).toHaveText('true');
+  });
 
-    // should show expected identity claims
-    await expect(getShownDebugValue('identityClaims')).toContainText('"iss": "https://demo.duendesoftware.com"');
-    await expect(getShownDebugValue('identityClaims')).toContainText('"name": "Bob Smith"');
+  test('should show expected identity claims', async () => {
+    await expect(page.locator('#identityClaims')).toContainText('"iss": "https://demo.duendesoftware.com"');
+    await expect(page.locator('#identityClaims')).toContainText('"name": "Bob Smith"');
+  });
 
-    // should be able to navigate to Admin-1 page
-    await clickMenuLinkTo('basics/admin1');
+  test('should be able to navigate to Admin-1 page', async () => {
+    await page.locator(`nav a[href="/basics/admin1"]`).click()
     await expect(page.locator('h1')).toContainText('Welcome');
-    await expect(getMainContentText()).toContainText('ADMIN');
-    await expect(getMainContentText()).toContainText('API Success');
+    await expect(page.locator('.container-fluid p.alert')).toContainText('ADMIN');
+    await expect(page.locator('.container-fluid p.alert')).toContainText('API Success');
+  });
 
-    // should be able to log out via IDS4
+  test('should be able to log out via IDS4', async () => {
     await page.locator('.btn-primary', { hasText: 'logout' }).click();
     await expect(page.getByText('now logged out')).toBeVisible();
+  });
 
-    // should be able to return to the app
+  test('should be able to return to the app', async () => {
     await page.locator('.PostLogoutRedirectUri').click();
+  });
 
-    // should initialize the application again
-    // (happens automatically in Playwright)
-
-    // should see user is not logged in
-    await expect(getShownDebugValue('isAuthenticated')).toHaveText('false');
-    await expect(getShownDebugValue('hasValidToken')).toHaveText('false');
-    await expect(getShownDebugValue('isDoneLoading')).toHaveText('true');
-    await expect(getShownDebugValue('canActivateProtectedRoutes')).toHaveText('false');
+  test('should see user is not logged in at the end', async () => {
+    await expect(page.locator('#isAuthenticated')).toHaveText('false');
+    await expect(page.locator('#hasValidToken')).toHaveText('false');
+    await expect(page.locator('#isDoneLoading')).toHaveText('true');
+    await expect(page.locator('#canActivateProtectedRoutes')).toHaveText('false');
   });
 
   test.afterEach(async () => {
